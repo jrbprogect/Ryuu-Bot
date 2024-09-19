e2a1a8b160793ad72848d88f2ff8509a3a76d0a3
jrbprogect <busacojr6@gmail.com>
2024-09-19 12:07:44 +0000

    Test

--- commands/spotify.js
+++ commands/spotify.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 const { convertToGothic } = require('../fontUtils');
 
 module.exports = {
@@ -15,22 +17,34 @@ module.exports = {
         }
 
         const trackName = args.join(' ');
-        const apiUrl = `http://spotifydl-api-54n8.onrender.com/download/6Csrqur3IfnVp0EtHskjMw/${encodeURIComponent(trackName)}.mp3`;
+        const searchUrl = `https://spotifydl-api-54n8.onrender.com/spotifydl?search=${encodeURIComponent(trackName)}`;
 
         await api.sendMessage(convertToGothic("Searching for your song, please wait..."), threadID, messageID);
 
         try {
-            const response = await axios.get(apiUrl);
+            const searchResponse = await axios.get(searchUrl);
 
-            if (response.data && response.data.track) {
-                const { name, artist, album, downloadLink } = response.data.track;
-                const duration = Math.floor(response.data.track.duration / 1000);
+            if (searchResponse.data && searchResponse.data.track) {
+                const { name, artist, album, downloadLink } = searchResponse.data.track;
+                const duration = Math.floor(searchResponse.data.track.duration / 1000);
                 const minutes = Math.floor(duration / 60);
                 const seconds = duration % 60;
                 const formattedDuration = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-                const message = `Track: ${name}\nArtist: ${artist}\nAlbum: ${album}\nDuration: ${formattedDuration}\nDownload Link: ${downloadLink}`;
+
+                const message = `Track: ${name}\nArtist: ${artist}\nAlbum: ${album}\nDuration: ${formattedDuration}\nDownloading the track...`;
 
                 await api.sendMessage(convertToGothic(message), threadID, messageID);
+
+                const filePath = path.join(__dirname, `${name}.mp3`);
+                const fileResponse = await axios.get(downloadLink, { responseType: 'arraybuffer' });
+                fs.writeFileSync(filePath, fileResponse.data);
+
+                await api.sendMessage({
+                    body: "Here is your song!",
+                    attachment: fs.createReadStream(filePath)
+                }, threadID, messageID);
+
+                fs.unlinkSync(filePath);
             } else {
                 await api.sendMessage(convertToGothic("No results found. Please try again."), threadID, messageID);
             }
